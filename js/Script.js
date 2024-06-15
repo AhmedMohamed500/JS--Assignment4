ff4a066c7a51b841403cc3603799b9b09f28d9f2
unknown <ahmed202040@gmail.com>
2024-06-15 19:49:15 +0300

    js commit

---
+++ js/Script.js
@@ -0,0 +1,25 @@
+var userName = document.querySelector("userName")
+var firstInput = document.querySelector("#first");
+var secondInput = document.querySelector("#second");
+var login = document.querySelector(".login")
+var signup = document.querySelector(".signup")
+
+
+
+login.addEventListener("click",function(){
+
+    if(firstInput = null){
+        console.log("not valid");
+
+    }else if (secondInput = null);{
+        console.log("not valid");
+    }
+
+})
+
+
+
+
+
+
+
